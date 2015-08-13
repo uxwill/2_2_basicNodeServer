32410782bcf637781a74caccf163479888a34cb5
uxwill <wfsaunders@gmail.com>
2015-08-13 15:45:41 +0200

    2

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.route('/me').get(function(req, res){
+app.route('/weather').get(function(req, res){
 
-res.sendFile(__dirname+'/public/html/contacts.html')
+res.sendFile(__dirname+'/public/weather/index.html');
 
 
-})
+});
+
+app.route('/weatherReport').get(function(req, res){
+
+res.sendFile(__dirname+'/public/weatherReport/index.html');
+
+
+});
 
 app.use(function(req, res, next){
 
 // res.type('txt').send('Not found');
 // res.status(404).end('error');
 
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
+  // if (req.accepts('html')) {
+  //   res.render('404', { url: req.url });
+  //   return;
+  // }
+
+res.sendFile(__dirname+'/public/404/index.html');
 
 });
 
